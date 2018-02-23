f617f325ae5b70bb1e3b2fdc90aeefab3dbd9a52
searene <mycityofsky@gmail.com>
2018-02-23 22:41:49 +0800

    use WordMap, which is faster

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { WordCandidate } from './index';
 import {EventEmitter} from 'events';
 import {AccentConverter} from './AccentConverter';
 import {DictMap, DictionaryFinder, IDictionary, IndexMap, WordForms} from './DictionaryFinder';
@@ -22,6 +23,7 @@ export class DictParser extends EventEmitter {
 
   private _wordformsMap: { [lang: string]: WordForms } = {};
   private _resourcePath: string;
+  private _wordMap: Map<string, Set<string>>;
 
   constructor(dbPath: string = DB_PATH,
               wordFormsFolder: string = WORD_FORMS_PATH,
@@ -35,6 +37,17 @@ export class DictParser extends EventEmitter {
     this._wordFormsFolder = wordFormsFolder;
   }
 
+  init = async (): Promise<void> => {
+    await this.buildWordList();
+  };
+
+  buildWordList = async (dictMapList?: DictMap[]) => {
+    if(dictMapList === undefined) {
+      dictMapList = await this.readDictMapListFromFile();
+    }
+    this._wordMap = this.getWordMap(dictMapList); 
+  };
+
   async scan(scanFolder: string | string[]): Promise<DictMap[]> {
     this._dictionaryFinder.on('name', (dictionaryName: string) => {
       this.emit('name', dictionaryName);
@@ -43,83 +56,35 @@ export class DictParser extends EventEmitter {
       scanFolder,
       this._dbPath,
       this._wordFormsFolder);
+    await this.buildWordList(this._dictMapList);
     return this._dictMapList;
   }
 
   /**
    * Guess what word the user wants based on the word input
    */
-  async getWordCandidates(input: string, resultCount = 30): Promise<WordCandidate[]> {
-    let wordSuggestions: {
-      input: string,
-      word: string,
-      posInFullWord: number,
-      dict: IDictionary,
-      isTransformed: boolean
-    }[] = [];
-    let normalizedInput = this.normalize(input);
-    if (this._dictMapList == undefined) this._dictMapList = await this.readDictMapListFromFile();
-
-    // check each dictionary
-    for (let dictMap of this._dictMapList) {
-
-      // check both originalWords and transformedWords
-      for (let words of [dictMap.originalWords, dictMap.transformedWords]) {
-
-        // check each word
-        for (let word in words) {
-          if (words.hasOwnProperty(word)) {
-            // a word is found
-            let normalizedWord = this.normalize(word);
-            let posInFullWord = normalizedWord.indexOf(normalizedInput);
-            if (posInFullWord > -1) {
-              wordSuggestions.push({
-                input: input,
-                word: word,
-                posInFullWord: posInFullWord,
-                dict: dictMap.dict,
-                isTransformed: words == dictMap.transformedWords
-              });
-              // if(wordSuggestions.length >= resultCount) {
-              //   return wordSuggestions.map(wordSuggestion => ({
-              //     word: wordSuggestion.word,
-              //     dict: wordSuggestion.dict
-              //   }));
-              // }
-            }
-          }
+  async getWordCandidates(input: string, resultCount = 30): Promise<string[]> {
+    let result: Set<string> = new Set<string>();
+    const BreakError = {};
+    input = this.normalize(input);
+    if(this._wordMap.has(input)) {
+      this._wordMap.get(input)!.forEach(originalWord => {
+        result.add(originalWord);
+      })
+    }
+    try {
+      this._wordMap.forEach((originalWords, normalizedWord) => {
+        if(normalizedWord.indexOf(input) === 0) {
+          originalWords.forEach(w => result.add(w));
         }
-      }
-    }
-    wordSuggestions.sort((a, b) => {
-      // sort originalWords before transformedWords
-      if (!a.isTransformed && b.isTransformed) {
-        return -1;
-      } else if (a.isTransformed && !b.isTransformed) {
-        return 1;
-      }
-
-      let lenDifferenceA = a.word.length - a.input.length;
-      let lenDifferenceB = b.word.length - b.input.length;
-      if (a.posInFullWord < b.posInFullWord || (a.posInFullWord == b.posInFullWord && lenDifferenceA < lenDifferenceB)) {
-        // a comes first
-        return -1
-      } else if (a.posInFullWord > b.posInFullWord || (a.posInFullWord == b.posInFullWord && lenDifferenceA > lenDifferenceB)) {
-        // b comes first
-        return 1
-      } else {
-        return 0;
-      }
-    });
-    let result: { word: string, dict: IDictionary }[] = [];
-    for (let i = 0; i < wordSuggestions.length; i++) {
-      if (i >= resultCount) break;
-      result.push({
-        word: wordSuggestions[i].word,
-        dict: wordSuggestions[i].dict
+        if(result.size >= resultCount) {
+          throw BreakError;
+        }
       });
+    } catch(e) {
+      if(e !== BreakError) throw e;
     }
-    return result;
+    return Array.from(result).slice(0, resultCount);
   }
 
   async getWordDefinitions(word: string): Promise<WordDefinition[]> {
@@ -155,9 +120,13 @@ export class DictParser extends EventEmitter {
   }
 
   async readDictMapListFromFile(): Promise<DictMap[]> {
-    let dbContents: string = await fse.readFile(this._dbPath, {encoding: 'utf8'});
-    const dictMapList = JSON.parse(dbContents) as DictMap[];
-    return dictMapList;
+    if(await fse.pathExists(this._dbPath)) {
+      let dbContents = await fse.readFile(this._dbPath, {encoding: 'utf8'});
+      const dictMapList = JSON.parse(dbContents) as DictMap[];
+      return dictMapList;
+    } else {
+      return [];
+    }
   }
 
   setDictMapList = (dictMapList: DictMap[]): void => {
@@ -170,6 +139,25 @@ export class DictParser extends EventEmitter {
       normalizedWord += AccentConverter.removeAccent(c).toLowerCase();
     }
     return normalizedWord;
+  }
+  private getWordMap = (dictMapList: DictMap[]): Map<string, Set<string>> => {
+    // normalized words => original words
+    const wordMap = new Map<string, Set<string>>();
+    for(const dictMap of dictMapList) {
+      for(const words of [dictMap.originalWords, dictMap.transformedWords]) {
+        for(const word in words) {
+          const normalizedWord = this.normalize(word);
+          if(wordMap.has(normalizedWord)) {
+            wordMap.get(normalizedWord)!.add(word);
+          } else {
+            const originalWordSet = new Set<string>();
+            originalWordSet.add(word);
+            wordMap.set(normalizedWord, originalWordSet);
+          }
+        }
+      }
+    }
+    return wordMap;
   }
 
 }
